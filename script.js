5258014de2bd1d082f91bddda739e289726df3f9
matheusclementeg <matheusclementeg@icloud.com>
2016-09-29 09:51:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function mostraModos() {
+	var texto = document.getElementById("text-mod");
+
+	// Changes the style to show the text
+	if (texto.style.display == "block") {
+		texto.style.display = "none";
+	} else {
+		texto.style.display = "block";
+	}
+}
+function mostraDicas() {
+	var texto = document.getElementById("text-dicas");
+
+	// Changes the style to show the text
+	if (texto.style.display == "block") {
+		texto.style.display = "none";
+	} else {
+		texto.style.display = "block";
+	}
+}
+
+function mostraCombinações() {
+	var texto = document.getElementById("text-combinacoes");
+
+	// Changes the style to show the text
+	if (texto.style.display == "block") {
+		texto.style.display = "none";
+	} else {
+		texto.style.display = "block";
+	}
+}
